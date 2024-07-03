c0814b047e20d14b644f173e49be0b6779e97222
Subx1s0o <nazarsabardin@gmail.com>
2024-07-03 16:16:27 +0300

    add styles

--- api/index.js
+++ api/index.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
@@ -59,9 +58,7 @@ app.post("/sendmail", (req, res, next) => {
     .sendMail(mailOptions)
     .then((info) => {
       console.log("Email sent: ", info);
-      res.send(
-        "Sent, check your email box. If you don't see it, check the spam folder."
-      );
+      res.render("message");
     })
     .catch((err) => {
       console.error("Error while sending email: ", err);
