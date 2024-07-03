07b96391e2add8051f620ce60a296a41d251c1b6
Subx1s0o <nazarsabardin@gmail.com>
2024-07-03 15:38:14 +0300

    change files paths

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
