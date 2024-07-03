bc8c633ea37152e7b06bc721c730d017a3523daf
Subx1s0o <nazarsabardin@gmail.com>
2024-07-03 15:12:25 +0300

    .

--- api/index.js
+++ api/index.js
@@ -5,23 +5,30 @@ import { config } from "dotenv";
 import path from "path";
 import hbs from "nodemailer-express-handlebars";
 import { fileURLToPath } from "url";
+
 config();
+
 const app = express();
 const PORT = 3000;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
+
 app.get("/", (req, res) => {
   res.render("index");
 });
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.listen(PORT, () => {
   console.log("server running on port: ", PORT);
 });
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
-
   auth: {
     user: "subx1s0o@gmail.com",
     pass: process.env.PASS,
@@ -30,11 +37,11 @@ const transporter = nodemailer.createTransport({
 
 const handlebarOpts = {
   viewEngine: {
-    extName: ".html",
-    partialsDir: path.resolve("./views"),
+    extName: ".handlebars",
+    partialsDir: path.join(__dirname, "../views"),
     defaultLayout: false,
   },
-  viewPath: path.resolve("./views"),
+  viewPath: path.join(__dirname, "../views"),
   extName: ".handlebars",
 };
 
@@ -42,6 +49,7 @@ transporter.use("compile", hbs(handlebarOpts));
 
 app.post("/sendmail", (req, res) => {
   const { text, email } = req.body;
+
   const mailOptions = {
     from: "subx1s0o@gmail.com",
     to: email,
@@ -51,17 +59,18 @@ app.post("/sendmail", (req, res) => {
       message: text,
     },
   };
+
   transporter
     .sendMail(mailOptions)
     .then((info) => {
-      console.log(info);
+      console.log("Email sent: ", info);
       res.send(
-        "Sended, check your email box. If you dont see, check spam folder"
+        "Sended, check your email box. If you don't see it, check the spam folder."
       );
     })
     .catch((err) => {
-      console.log(err);
-      res.send("error while sending", err);
+      console.error("Error while sending email: ", err);
+      res.status(500).send("Error while sending.");
     });
 });
 
