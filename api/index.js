e9b2937629114e03497137e0ac13f3ca5f1206cf
Subx1s0o <nazarsabardin@gmail.com>
2024-07-03 15:53:27 +0300

    .

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from "url";
 config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,10 +19,6 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -71,7 +67,6 @@ app.post("/sendmail", (req, res, next) => {
     });
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log("Server running on port: ", PORT);
 });
